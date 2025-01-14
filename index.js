51098bf536d39949b08c3534896065834d41eb4e
Rakesh-satyavarapu <rakeshsatyavarapu369@gmail.com>
2025-01-14 13:54:34 +0530

    final commit

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+let dns = require('dns')
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -19,6 +22,41 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+const urlDatabase = {};
+let id = 1;
+
+app.post('/api/shorturl',(req,res)=>{
+  let url = req.body.url
+  if(!isValidUrl(url))
+  {
+    res.json( { error: 'invalid url' })
+  }
+  let hostname = new URL(url).hostname;
+  dns.lookup(hostname,(err)=>{
+    if(err)
+    {
+      res.json({ error: 'invalid url' })
+    }
+    else{
+      let shortUrl = id++;
+      urlDatabase[shortUrl] = url;
+      res.json({ original_url: url, short_url: shortUrl });
+    }
+  })
+})
+
+app.get('/api/shorturl/:id',(req,res)=>{
+  let id  = req.params.id
+  res.redirect(urlDatabase[id])
+})
+
+const isValidUrl = (url) => {
+  try {
+    const urlObject = new URL(url);
+    return ['http:', 'https:'].includes(urlObject.protocol);
+  } catch (err) {
+    return false;
+  }}
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
